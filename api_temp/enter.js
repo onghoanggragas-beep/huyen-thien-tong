de7d1e29509013ee842971140681ab241b79ac3b
Kira Shu <onghoanggragas@gmail.com>
2026-01-03 03:44:30 +0700

    Delete api_temp directory

--- api_temp/enter.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  const playerId = crypto.randomUUID();
-  const roots = ["Kim","Mộc","Thủy","Hỏa","Thổ","Dị"];
-  const root = roots[Math.floor(Math.random()*roots.length)];
-  res.json({ playerId, intro:`Ngươi mang ${root} linh căn, bước vào Huyền Thiên Tông.`});
-}
