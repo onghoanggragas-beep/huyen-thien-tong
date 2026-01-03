b3229eb450d3fafb90a0ae5611df82a5d8c8c2c6
Kira Shu <onghoanggragas@gmail.com>
2026-01-03 14:28:36 +0700

    Update enter.js

--- functions/enter.js
+++ functions/enter.js
@@ -1,6 +1,5 @@
 export async function onRequest() {
-  return new Response(
-    JSON.stringify({ intro: "Ngươi bước vào Huyền Thiên Tông." }),
-    { headers: { "Content-Type": "application/json" } }
-  );
+  return new Response(JSON.stringify({
+    intro: "Ngươi bước vào Huyền Thiên Tông. Con đường tu tiên bắt đầu."
+  }), { headers: { "Content-Type": "application/json" }});
 }
