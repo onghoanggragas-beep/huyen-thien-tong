64558d2db4860eca039f2d06d2acf8267015bb5c
Kira Shu <onghoanggragas@gmail.com>
2026-01-03 16:22:30 +0700

    Delete functions directory

--- functions/talk.js
+++
@@ -1,6 +0,0 @@
-export async function onRequest({ request }) {
-  const { message } = await request.json();
-  return Response.json({
-    reply: `📜 Tông môn ghi nhận lời nói: "${message}"`
-  });
-}
