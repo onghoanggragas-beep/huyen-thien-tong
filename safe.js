88161e5667db3adf08ca303702502fbb45bddb17
Kira Shu <onghoanggragas@gmail.com>
2026-01-04 00:52:41 +0700

    Update safe.js

--- safe.js
+++ safe.js
@@ -1,27 +1,3 @@
-// ===== SAFE PLACEHOLDER – CHỐNG GAME SẬP =====
-
-// render
-if (typeof renderCharacterInfo !== "function") {
-  window.renderCharacterInfo = function(){};
-}
-
-// công pháp
-if (typeof learnCultivationMethod !== "function") {
-  window.learnCultivationMethod = function(){
-    alert("Hệ công pháp đang xây dựng");
-  };
-}
-
-// battle
-if (typeof startBattle !== "function") {
-  window.startBattle = function(){
-    alert("Chiến đấu chưa sẵn sàng");
-  };
-}
-
-// inventory
-if (typeof openInventory !== "function") {
-  window.openInventory = function(){
-    alert("Kho đồ chưa sẵn sàng");
-  };
-}
+if(typeof startBattle!=="function") window.startBattle=function(){};
+if(typeof openInventory!=="function") window.openInventory=function(){};
+if(typeof learnCultivationMethod!=="function") window.learnCultivationMethod=function(){};
