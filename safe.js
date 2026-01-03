91bc0203fe0f1818084148c9b2d71aa07c5c59c9
Kira Shu <onghoanggragas@gmail.com>
2026-01-04 02:32:44 +0700

    Delete safe.js

--- safe.js
+++
@@ -1,3 +0,0 @@
-if(typeof startBattle!=="function") window.startBattle=function(){};
-if(typeof openInventory!=="function") window.openInventory=function(){};
-if(typeof learnCultivationMethod!=="function") window.learnCultivationMethod=function(){};
