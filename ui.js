3e6d52c70d2734ea1e29524e2e3872d5f4adfce6
Kira Shu <onghoanggragas@gmail.com>
2026-01-04 16:14:46 +0700

    Create ui.js

---
+++ ui.js
@@ -0,0 +1,18 @@
+/* =====================================================
+   UI ACCORDION MENU
+===================================================== */
+
+function toggleMenu(id) {
+  var menus = document.querySelectorAll(".menu-content");
+
+  for (var i = 0; i < menus.length; i++) {
+    if (menus[i].id !== id) {
+      menus[i].classList.add("hidden");
+    }
+  }
+
+  var target = document.getElementById(id);
+  if (!target) return;
+
+  target.classList.toggle("hidden");
+}
